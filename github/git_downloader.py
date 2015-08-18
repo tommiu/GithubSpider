514e1036f26f1785cbb09f4266fc5a5d1e3a5527
Tommi <tommiu@web.de>
2015-08-18 15:00:26 +0200

    goToLine now raises exception on EOF.

--- github/git_downloader.py
+++ github/git_downloader.py
@@ -13,7 +13,14 @@ class GitDownloader(object):
     """
     Manages the download of git repositories.
     """
+    def __init__(self, dir_path):
+        self.OUT_DIR = dir_path
+    
+        if self.OUT_DIR[-1] != "/":
+            self.OUT_DIR += "/"
+                
     def cloneAllFromFile(self, filename, linenumber=0):
+        linenumber = int(linenumber)
         with open(filename, 'r') as fh:
             if linenumber > 1:
                 self.goToLine(fh, linenumber)
@@ -21,31 +28,46 @@ class GitDownloader(object):
             l = fh.readline()
             while l:
                 try:
+                    print "Trying link on line %d in file '%s'" % (linenumber, 
+                                                                   filename)
                     self.cloneRepoLink(l.strip())
                 
-                except RepositoryExistsException as err:
-                    print err
-                    print "Skipping to next link in file."
+                except (
+                    RepositoryExistsException, 
+                    RepositoryDoesNotExistException
+                    ) as err:
+                        print str(err).strip()
+                        print "Skipping..."
                     
                 finally:
                     linenumber += 1
+                    l = fh.readline()
+                    
+            print "End of file reached, my work is done!"
             
     def cloneRepoLink(self, link):
-        msg = "Cloning repository: %s..." % link
-        
+        msg     = "Cloning repository: %s..." % link
+        out_dir = self.OUT_DIR + link[link.rfind("/") + 1 : -4]
+
         print "%s\r" % msg,
         sys.stdout.flush()
-        process = subprocess.Popen(["git", "clone", link], 
+        process = subprocess.Popen(["git", "clone", link, out_dir], 
                                    stdout=subprocess.PIPE, 
                                    stderr=subprocess.PIPE)
         stdout, stderr = process.communicate()
         
         if stderr:
+            print "%s Failed." % msg
             if "already exists and is not an empty directory." in stderr:
                 raise RepositoryExistsException(str(stderr))
-        
+            
+            elif "does not exist" in stderr:
+                raise RepositoryDoesNotExistException(str(stderr))
+            
         print "%s Done." % msg
-        print stdout
+        
+        if stdout:
+            print stdout
     
     def goToLine(self, fh, linenumber):
         """
@@ -57,9 +79,35 @@ class GitDownloader(object):
                 )
         
         fh.seek(0, os.SEEK_SET)
+
+        # Skip lines until desired line is reached.
         for _ in range(0, linenumber - 1):
-            fh.readline()
+            read = fh.readline()
+            if read == "":
+                # Empty string represents EOF.
+                raise OutOfScopeException(msg="goToLine error: ", 
+                                          line=linenumber)
             
 
 class RepositoryExistsException(BaseException):
     pass
+
+class RepositoryDoesNotExistException(BaseException):
+    pass
+
+class OutOfScopeException(BaseException):
+    def __init__(self, msg=None, line=None):
+        if msg:
+            self.message = msg
+            
+        if line:
+            self.message += "Line %d is out of scope." % line
+            
+        else:
+            self.message = (
+                    "goToLine() was called with a linenumber, "
+                    "which was out of scope."
+                    )
+        
+    def __str__(self):
+        return self.message
